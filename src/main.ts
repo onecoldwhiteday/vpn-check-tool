bf9d265e02366bb812193b4a4e8588e4d48adbc2
nrychagov <nikita.rychagov@noveogroup.com>
2022-10-11 17:34:32 +0600

    fix: updates port

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/doc', app, document);
 
-  await app.listen(3000);
+  await app.listen(8081);
 }
 bootstrap();
