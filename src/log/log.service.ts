5d9f85808d7a4fe91cd4c969edab18349f647c2a
nrychagov <nikita.rychagov@noveogroup.com>
2022-10-17 18:53:44 +0600

    feat: updates send log endpoint with new request url

--- src/log/log.service.ts
+++ src/log/log.service.ts
@@ -8,17 +8,17 @@ export class LogService {
   constructor(
     private config: ConfigService,
     private readonly httpService: HttpService,
-  ) { }
+  ) {}
 
   async sendLog(logData: Partial<ConnectionLogDTO>) {
-    const log = this.generateLog(logData);
-    const url = `${this.config.get(
-      'WEB_BACK_URL',
-    )}/api/projects/connection-logs`;
+    const log = LogService.generateLog(logData);
+    const url = `${this.config.get('WEB_BACK_URL')}/api/connection-logs`;
     return this.httpService.post(url, log);
   }
 
-  private generateLog(logData: Partial<ConnectionLogDTO>): ConnectionLogDTO {
+  private static generateLog(
+    logData: Partial<ConnectionLogDTO>,
+  ): ConnectionLogDTO {
     const createdAt = Date.now().toLocaleString();
     return {
       username: logData.username,
